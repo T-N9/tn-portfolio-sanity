1c5b9bcd5f0999745101515f1c7dadbed707a8df
T-N9 <tenyainmoelwin@gmail.com>
2022-02-25 17:19:03 +0630

    Added commit

---
+++ schemas/projects.js
@@ -0,0 +1,49 @@
+export default {
+    name : 'projects',
+    title : 'Projects',
+    type : 'document',
+    fields:[
+        {
+            name:'title',
+            title:'Title',
+            type:'string'
+        },
+        {
+            name:'description',
+            title:'Description',
+            type:'string'
+        },
+        {
+            name : 'category',
+            title : 'Category',
+            type : 'string'
+        },
+        {
+            name : 'icon',
+            title : 'Icon',
+            type : 'string'
+        },
+        {
+            name:'imgUrl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },
+        {
+            name: "order",
+            title: "Order",
+            type: "number",
+            hidden: true,
+        },
+        
+    ],
+    orderings: [
+        {
+          title: "Manual order",
+          name: "manualOrder",
+          by: [{ field: "order", direction: "asc" }],
+        },
+    ],
+}
