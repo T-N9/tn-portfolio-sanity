026605f12ea379729fd2a2bffb37b4532d48f7c3
T-N9 <tenyainmoelwin@gmail.com>
2022-03-11 08:42:27 +0630

    Updated attri text

--- schemas/projects.js
+++ schemas/projects.js
@@ -66,12 +66,6 @@ export default {
             type: 'string',
         },
         {
-            name:'introduction',
-            title:'Introduction',
-            type: 'array', 
-            of: [{type: 'block'}]
-        },
-        {
             name: "order",
             title: "Order",
             type: "number",
